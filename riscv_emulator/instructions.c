ea6be3381f071cb3d505b9362759bf7f3b563614
bergschaf <christian.k@netcom-mail.de>
2024-01-07 15:08:38 +0100

    templates

--- riscv_emulator/instructions.c
+++ riscv_emulator/instructions.c
@@ -72,3 +72,155 @@ void print_Instruction(Instruction inst){
 void execute_lui(Cpu *cpu, Instruction inst){
     cpu->regs[inst.rd] = inst.imm;
 }
+
+void execute_auipc(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_jal(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_jalr(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_beq(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_bne(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_blt(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_bge(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_bltu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_bgeu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_lb(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_lh(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_lw(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_lbu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_lhu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sb(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sh(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sw(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_addi(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_slti(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sltiu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_xori(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_ori(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_andi(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_slli(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_srli(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_srai(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_add(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sub(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sll(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_slt(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sltu(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_xor(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_srl(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_sra(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_or(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_and(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_fence(Cpu *cpu, Instruction inst){
+    //
+}
+
+void execute_ecall(Cpu *cpu, Instruction inst){
+    //
+}
