9d86611b0f96524efb2548c05debfd604344c4ee
bergschaf <christian.k@netcom-mail.de>
2024-01-12 22:46:11 +0100

    committtttttt

--- arduino_zuig/main.c
+++ arduino_zuig/main.c
@@ -1,20 +1,23 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdio.h>
-#define MS_DELAY 100
+#define MS_DELAY 300
 
 int main (void) {
     /*Set to one the fifth bit of DDRB to one
     **Set digital pin 13 to output mode */
     DDRB |= _BV(DDB5);
+
+    // Enable Transmitter
+    UBRRH = UBRR_VAL >> 8;
+    UBRRL = UBRR_VAL & 0xFF;
+
+    UCSRB |= (1<<TXEN);  // UART TX einschaltentest.c
+    UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);  // Asynchron 8N1
+
     // Set frame format: 8data, 1stop bit
-    uart_init();
+	
     while(1) {
-        // print hello world to serial with baudrate 9600
-        printf("Hello world\n");
-
-
-
+	printf("Hello world\n");
         /*Set to one the fifth bit of PORTB to one
         **Set to HIGH the pin 13 */
         PORTB |= _BV(PORTB5);
