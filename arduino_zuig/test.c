0eae61ca6e6033090f8f9a49d3a6bb7ab2ca3c00
bergschaf <christian.k@netcom-mail.de>
2024-01-12 22:56:27 +0100

    serial output

--- arduino_zuig/test.c
+++ arduino_zuig/test.c
@@ -1,32 +1,67 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdio.h>
-#define MS_DELAY 100
+/*
+* usart.c
+*
+* Created : 15-08-2020 07:24:45 PM
+* Author  : Arnab Kumar Das
+* Website : www.ArnabKumarDas.com
+*/
 
-int main (void) {
-    /*Set to one the fifth bit of DDRB to one
-    **Set digital pin 13 to output mode */
-    DDRB |= _BV(DDB5);
-    // Set frame format: 8data, 1stop bit
-    uart_init();
-    while(1) {
-        // print hello world to serial with baudrate 9600
-        printf("Hello world\n");
+#define F_CPU 16000000UL // Defining the CPU Frequency
 
+#include <avr/io.h>      // Contains all the I/O Register Macros
+#include <util/delay.h>  // Generates a Blocking Delay
 
+#define USART_BAUDRATE 9600 // Desired Baud Rate
+#define BAUD_PRESCALER (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
-        /*Set to one the fifth bit of PORTB to one
-        **Set to HIGH the pin 13 */
-        PORTB |= _BV(PORTB5);
+#define ASYNCHRONOUS (0<<UMSEL00) // USART Mode Selection
 
-        /*Wait 3000 ms */
-        _delay_ms(MS_DELAY);
+#define DISABLED    (0<<UPM00)
+#define EVEN_PARITY (2<<UPM00)
+#define ODD_PARITY  (3<<UPM00)
+#define PARITY_MODE  DISABLED // USART Parity Bit Selection
 
-        /*Set to zero the fifth bit of PORTB
-        **Set to LOW the pin 13 */
-        PORTB &= ~_BV(PORTB5);
+#define ONE_BIT (0<<USBS0)
+#define TWO_BIT (1<<USBS0)
+#define STOP_BIT ONE_BIT      // USART Stop Bit Selection
 
-        /*Wait 3000 ms */
-        _delay_ms(MS_DELAY);
+#define FIVE_BIT  (0<<UCSZ00)
+#define SIX_BIT   (1<<UCSZ00)
+#define SEVEN_BIT (2<<UCSZ00)
+#define EIGHT_BIT (3<<UCSZ00)
+#define DATA_BIT   EIGHT_BIT  // USART Data Bit Selection
+
+void USART_Init()
+{
+    // Set Baud Rate
+    UBRR0H = BAUD_PRESCALER >> 8;
+    UBRR0L = BAUD_PRESCALER;
+
+    // Set Frame Format
+    UCSR0C = ASYNCHRONOUS | PARITY_MODE | STOP_BIT | DATA_BIT;
+
+    // Enable Receiver and Transmitter
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+}
+
+void USART_TransmitPolling(uint8_t DataByte)
+{
+    while (( UCSR0A & (1<<UDRE0)) == 0) {}; // Do nothing until UDR is ready
+    UDR0 = DataByte;
+}
+
+int main()
+{
+    USART_Init();
+    while (1)
+    {
+        USART_TransmitPolling('A');
+        USART_TransmitPolling('R');
+        USART_TransmitPolling('N');
+        USART_TransmitPolling('A');
+        USART_TransmitPolling('B');
+        USART_TransmitPolling('\n');
+        _delay_ms(1000);
     }
+    return 0;
 }
